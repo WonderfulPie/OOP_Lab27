65858a1719a0e47941ad6299f75c284ecfdd4825
Viktoriia Danylko <danylkovika1405@gmail.com>
2024-06-13 14:42:08 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Lab_27_Danylko
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(200, 426);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(218, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(300, 200);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;  // Виправлення помилки 
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Type";
+            this.columnHeader2.Width = 150;
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Location = new System.Drawing.Point(524, 12);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(250, 426);
+            this.propertyGrid1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 218);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 220);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;  // Приховуємо TextBox за замовчуванням
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(218, 218);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 220);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;  // Приховуємо PictureBox за замовчуванням
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "All",
+            "File",
+            "Directory"});
+            this.comboBox1.Location = new System.Drawing.Point(218, 444);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(300, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 481);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "File Explorer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Lab_27_Danylko
+{
+    public partial class Form1 : Form
+    {
+        private ImageList imageList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeImageList();
+            LoadDrives();
+            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+        }
+
+        private void InitializeImageList()
+        {
+            imageList = new ImageList();
+            imageList.ImageSize = new Size(16, 16);
+
+            try
+            {
+                // Завантаження іконок
+                imageList.Images.Add("drive", ShellIcon.GetSmallIcon("shell32.dll", 8).ToBitmap());
+                imageList.Images.Add("folder", ShellIcon.GetSmallIcon("shell32.dll", 3).ToBitmap());
+                imageList.Images.Add("file", ShellIcon.GetSmallIcon("shell32.dll", 0).ToBitmap()); // Іконка для невідомих файлів
+                imageList.Images.Add("text", Image.FromFile("Icons/text.png")); // Іконка для текстових файлів
+                imageList.Images.Add("image", Image.FromFile("Icons/image.png")); // Іконка для зображень
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"Icon file not found: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while loading icons: {ex.Message}");
+            }
+
+            treeView1.ImageList = imageList;
+            listView1.SmallImageList = imageList;
+        }
+
+        private void LoadDrives()
+        {
+            try
+            {
+                foreach (var drive in DriveInfo.GetDrives())
+                {
+                    TreeNode node = new TreeNode(drive.Name, 0, 0) { Tag = drive };
+                    treeView1.Nodes.Add(node);
+                    if (drive.IsReady)
+                        node.Nodes.Add(new TreeNode()); // Додаємо пустий вузол як маркер
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while loading drives: {ex.Message}");
+            }
+        }
+
+        private void LoadDirectoriesAndFiles(TreeNode node)
+        {
+            DirectoryInfo dir = node.Tag as DirectoryInfo ?? (node.Tag as DriveInfo)?.RootDirectory;
+            if (dir == null) return;
+
+            try
+            {
+                node.Nodes.Clear(); // Очищуємо існуючі вузли
+
+                foreach (var subDir in dir.GetDirectories())
+                {
+                    try
+                    {
+                        TreeNode subNode = new TreeNode(subDir.Name, 1, 1) { Tag = subDir };
+                        subNode.Nodes.Add(new TreeNode()); // Додаємо пустий вузол як маркер для можливості розгортання
+                        node.Nodes.Add(subNode);
+                    }
+                    catch (UnauthorizedAccessException) { }
+                    catch (DirectoryNotFoundException) { }
+                }
+
+                foreach (var file in dir.GetFiles())
+                {
+                    try
+                    {
+                        string imageKey = GetFileIconKey(file.Extension);
+                        TreeNode fileNode = new TreeNode(file.Name, imageKey == "text" ? 3 : imageKey == "image" ? 4 : 2, imageKey == "text" ? 3 : imageKey == "image" ? 4 : 2) { Tag = file };
+                        node.Nodes.Add(fileNode);
+                    }
+                    catch (UnauthorizedAccessException) { }
+                    catch (FileNotFoundException) { }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while loading directories and files: {ex.Message}");
+            }
+        }
+
+        private string GetFileIconKey(string extension)
+        {
+            switch (extension.ToLower())
+            {
+                case ".txt":
+                    return "text";
+                case ".jpg":
+                case ".jpeg":
+                case ".png":
+                case ".bmp":
+                case ".gif":
+                    return "image";
+                default:
+                    return "file";
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node = e.Node;
+            if (node.Nodes.Count == 1 && node.Nodes[0].Tag == null)
+            {
+                LoadDirectoriesAndFiles(node);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode selectedNode = e.Node;
+            propertyGrid1.SelectedObject = selectedNode.Tag;
+            listView1.Items.Clear();
+
+            if (selectedNode.Tag is DriveInfo drive)
+            {
+                if (drive.IsReady)
+                    LoadFilesAndDirectories(drive.RootDirectory);
+            }
+            else if (selectedNode.Tag is DirectoryInfo dir)
+            {
+                LoadFilesAndDirectories(dir);
+            }
+            else if (selectedNode.Tag is FileInfo file)
+            {
+                LoadFileContent(file);
+            }
+        }
+
+        private void LoadFilesAndDirectories(DirectoryInfo dir)
+        {
+            try
+            {
+                foreach (var subDir in dir.GetDirectories())
+                {
+                    try
+                    {
+                        ListViewItem item = new ListViewItem(subDir.Name, "folder") { Tag = subDir };
+                        item.SubItems.Add("Directory");
+                        listView1.Items.Add(item);
+                    }
+                    catch (UnauthorizedAccessException) { }
+                    catch (DirectoryNotFoundException) { }
+                }
+
+                foreach (var file in dir.GetFiles())
+                {
+                    try
+                    {
+                        string imageKey = GetFileIconKey(file.Extension);
+                        ListViewItem item = new ListViewItem(file.Name, imageKey) { Tag = file };
+                        item.SubItems.Add("File");
+                        listView1.Items.Add(item);
+                    }
+                    catch (UnauthorizedAccessException) { }
+                    catch (FileNotFoundException) { }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while loading files and directories: {ex.Message}");
+            }
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if (e.IsSelected)
+            {
+                propertyGrid1.SelectedObject = e.Item.Tag;
+                if (e.Item.Tag is FileInfo file)
+                {
+                    LoadFileContent(file);
+                }
+            }
+        }
+
+        private void LoadFileContent(FileInfo file)
+        {
+            try
+            {
+                if (file.Extension == ".txt")
+                {
+                    textBox1.Text = File.ReadAllText(file.FullName);
+                    textBox1.Visible = true;
+                    pictureBox1.Image = null;
+                    pictureBox1.Visible = false;
+                }
+                else if (file.Extension == ".jpg" || file.Extension == ".jpeg" || file.Extension == ".png" || file.Extension == ".bmp" || file.Extension == ".gif")
+                {
+                    pictureBox1.Image = Image.FromFile(file.FullName);
+                    pictureBox1.Visible = true;
+                    textBox1.Clear();
+                    textBox1.Visible = false;
+                }
+                else
+                {
+                    textBox1.Clear();
+                    textBox1.Visible = false;
+                    pictureBox1.Image = null;
+                    pictureBox1.Visible = false;
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"File not found: {ex.Message}");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"Access denied to file: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while loading file content: {ex.Message}");
+            }
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string filter = comboBox1.SelectedItem.ToString().ToLower();
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (filter == "all" || item.SubItems[1].Text.ToLower().Contains(filter))
+                {
+                    item.ForeColor = SystemColors.WindowText; // Відображати елемент
+                }
+                else
+                {
+                    item.ForeColor = SystemColors.GrayText; // Приховати елемент через зміну кольору тексту
+                }
+            }
+        }
+    }
+
+    public static class ShellIcon
+    {
+        [DllImport("Shell32.dll")]
+        public static extern int ExtractIconEx(string file, int index, IntPtr[] largeIcon, IntPtr[] smallIcon, int icons);
+
+        public static Icon GetSmallIcon(string file, int index)
+        {
+            IntPtr[] smallIcon = new IntPtr[1];
+            ExtractIconEx(file, index, null, smallIcon, 1);
+            return Icon.FromHandle(smallIcon[0]);
+        }
+    }
+}
